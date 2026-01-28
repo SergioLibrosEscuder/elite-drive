b82b3998855ffdaa68e1425050997db5c99c9744
Guille-git <guillesototorres@gmail.com>
2026-01-29 00:37:19 +0100

    Estructura básica de Index

--- resources/js/components/HeaderCustom.vue
+++ resources/js/components/HeaderCustom.vue
@@ -1,5 +1,27 @@
 <template>
+    <!-- HEADER / NAVBAR ============================================================================ -->
+
     <header>
-        <h1>Elite Drive</h1>
+        <nav class="navbar navbar-expand-lg sticky-top shadow-sm">
+            <div class="container">
+                <a class="navbar-brand fw-bold" href="index.php">ELITE DRIVE</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto align-items-center">
+                        <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="cars.html">Cars</a></li>
+                        <li class="nav-item"><a class="nav-link" href="gallery.html">Gallery</a></li>
+                        <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+                        <li class="nav-item ms-lg-3">
+                            <button class="btn bg-primary-cta btn-sm" data-bs-toggle="modal" data-bs-target="#loginModal">
+                                <i class="bi bi-person-circle me-2"></i> Login
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     </header>
 </template>
--- resources/js/pages/Index.vue
+++ resources/js/pages/Index.vue
@@ -1,7 +1,9 @@
 <script setup>
+import FooterCustom from "../components/FooterCustom.vue";
 import HeaderCustom from "../components/HeaderCustom.vue";
 </script>
 
 <template>
     <HeaderCustom />
+    <FooterCustom />
 </template>
