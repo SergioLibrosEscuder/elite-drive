44dfc4cdaed36db0bfe436f6b1134d89403d3f25
Sergio Libros Escuder <librosescudersergio@gmail.com>
2026-01-29 08:45:30 +0100

    Add Vue Router configuration and integrate with app

---
+++ resources/js/router/index.js
@@ -0,0 +1,11 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Index from "../pages/Index.vue";
+
+const routes = [{ path: "/", component: Index }];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
