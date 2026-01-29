52acc09cc52be9f7b7587294a51e66dd0198516c
Sergio Libros Escuder <librosescudersergio@gmail.com>
2026-01-29 09:26:35 +0100

    Refactor app structure: update main entry point to use App.vue and configure route handling

---
+++ resources/js/pages/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import FooterCustom from '../components/FooterCustom.vue';
+import HeaderCustom from '../components/HeaderCustom.vue';
+
+</script>
+
+<template>
+    <HeaderCustom />
+    <router-view />
+    <FooterCustom />
+</template>
