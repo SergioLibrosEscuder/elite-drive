ed8aaac55476317506dff0afaeee770ece2b4c12
Sergio Libros Escuder <librosescudersergio@gmail.com>
2026-02-01 19:36:33 +0100

    Added CRUD routes needed for reservations

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\VehicleController;
+use App\Http\Controllers\ReservationController;
 
 // API/Actions Routes
 Route::post('/login', [AuthController::class, 'login']);
@@ -12,10 +15,18 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::middleware('auth')->group(function () {
     Route::put('/user/profile', [AuthController::class, 'updateProfile']);
     Route::put('/user/password', [AuthController::class, 'updatePassword']);
+    Route::get('/user/reservations', [ReservationController::class, 'userReservations']);
     // Ruta para obtener los datos del usuario logueado al cargar la página
     Route::get('/user/me', function () {
         return Auth::user();
     });
+
+    Route::get('/reservations', [ReservationController::class, 'index']);
+    Route::get('/reservations/{id}', [ReservationController::class, 'show']);
+    Route::post('/reservations', [ReservationController::class, 'store']);
+    Route::put('/reservations/{id}', [ReservationController::class, 'update']);
+    Route::delete('/reservations/{id}', [ReservationController::class, 'destroy']);
+
     Route::get('/admin/customers', [AdminController::class, 'index']);
     Route::post('/admin/customers', [AdminController::class, 'store']);
     Route::put('/admin/customers/{id}', [AdminController::class, 'update']);
