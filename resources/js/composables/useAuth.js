d313d553ce3655db0f01a5d462da90b5e9954313
Sergio Libros Escuder <librosescudersergio@gmail.com>
2026-02-05 09:20:45 +0100

    Refactorized useAuth to only get user data if the user is logged in

--- resources/js/composables/useAuth.js
+++ resources/js/composables/useAuth.js
@@ -8,11 +8,19 @@ export function useAuth() {
     const fetchUser = async () => {
         if (isInitialized.value) return;
 
+        const hasLoginFlag = localStorage.getItem("is_logged_in");
+        if (!hasLoginFlag) {
+            user.value = null;
+            isInitialized.value = true;
+            return;
+        }
+
         try {
             const response = await axios.get("/user/me");
             user.value = response.data;
         } catch (error) {
             user.value = null;
+            localStorage.removeItem("is_logged_in");
         } finally {
             isInitialized.value = true;
         }
@@ -20,6 +28,9 @@ export function useAuth() {
 
     const login = async (formData) => {
         await axios.post("/login", formData);
+
+        localStorage.setItem("is_logged_in", "true");
+        isInitialized.value = false;
         await fetchUser();
     };
 
@@ -27,9 +38,9 @@ export function useAuth() {
         try {
             await axios.post("/logout");
         } catch (e) {
-            /* ignorar error */
+            /* Ignorar error */
         }
-
+        localStorage.removeItem("is_logged_in");
         user.value = null;
         isInitialized.value = true;
     };
