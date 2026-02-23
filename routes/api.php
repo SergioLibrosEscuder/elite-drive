99db5ad22f676033005bec516904228d3fd93f21
Sergio Libros Escuder <librosescudersergio@gmail.com>
2026-02-23 13:15:18 +0100

    Refactor: Add comments for clarity in API and web routes

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
+
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VehicleController;
 
 Route::post('/login', [AuthController::class, 'login']);
 
+// Public routes to get cars
 Route::get("/cars", [VehicleController::class, 'index']);
 Route::get("/cars/{id}", [VehicleController::class, 'show']);
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::get('/email/verify/{id}/{hash}', [AuthController::class, 'verify'])
 
 // Only accesible if authenticated user
 Route::middleware('auth')->group(function () {
+    // User profile routes
     Route::put('/user/profile', [AuthController::class, 'updateProfile']);
     Route::put('/user/password', [AuthController::class, 'updatePassword']);
     Route::get('/user/reservations', [ReservationController::class, 'userReservations']);
@@ -32,21 +33,23 @@ Route::middleware('auth')->group(function () {
     Route::get('/user/me', function () {
         return Auth::user();
     });
+    // Logout route
     Route::post('/logout', [AuthController::class, 'logout']);
-
+    // Admin routes for managing reservations
     Route::get('/reservations', [ReservationController::class, 'index']);
     Route::get('/reservations/{id}', [ReservationController::class, 'show']);
     Route::post('/reservations', [ReservationController::class, 'store']);
     Route::put('/reservations/{id}', [ReservationController::class, 'update']);
     Route::delete('/reservations/{id}', [ReservationController::class, 'destroy']);
+    // Routes for confirming and canceling reservations
     Route::put('/reservations/{id}/confirm', [ReservationController::class, 'confirm']);
     Route::put('/reservations/{id}/cancel', [ReservationController::class, 'cancel']);
-
+    // Admin routes for managing customers
     Route::get('/admin/customers', [AdminController::class, 'index']);
     Route::post('/admin/customers', [AdminController::class, 'store']);
     Route::put('/admin/customers/{id}', [AdminController::class, 'update']);
     Route::delete('/admin/customers/{id}', [AdminController::class, 'destroy']);
-
+    // Admin routes for managing cars
     Route::post('/cars', [VehicleController::class, 'store']);
     Route::put('/cars/{id}', [VehicleController::class, 'update']);
     Route::delete('/cars/{id}', [VehicleController::class, 'destroy']);
